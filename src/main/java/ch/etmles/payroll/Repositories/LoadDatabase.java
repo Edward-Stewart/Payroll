82e5e34871406f27fec92408d5db9eef35687170
Edward Stewart <pv00gab@eduvaud.ch>
2025-12-10 16:33:34 +0100

    yo

--- src/main/java/ch/etmles/payroll/Repositories/LoadDatabase.java
+++ src/main/java/ch/etmles/payroll/Repositories/LoadDatabase.java
@@ -16,7 +16,6 @@ public class LoadDatabase {
         return args->{
             log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
-            log.info("Preloading " + repository.save(new Employee("Donkey kong", "Ape")));
         };
     }
 }
